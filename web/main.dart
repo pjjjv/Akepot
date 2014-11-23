1f65c4adda9fc23080b96e808e62940e3b3f7213
pjv <ezelspinguin@gmail.com>
2014-11-23 16:49:48 +0100

    Colors and typography

--- web/main.dart
+++ web/main.dart
@@ -1,10 +1,15 @@
 import 'dart:html';
 import 'package:polymer/polymer.dart';
+import 'package:template_binding/template_binding.dart';
 
 void main() {
   initPolymer().run(() {
-    // The rest of the code in the main method.
-    querySelector('#menu_button').on['tap'].listen(openMenu);
+    //(querySelector('#menu_button') as PolymerElement).onClick.listen(openMenu);
+
+    Polymer.onReady.then((_) {
+      var content = document.querySelector('#content');
+      templateBind(content).model = new Content();
+    });
   });
 }
 
@@ -12,4 +17,13 @@ void openMenu(Event e) {
   print("openMenu");
   var dialog = querySelector('#dialog');
   dialog.toggle();
+}
+
+class Content extends Observable {
+  @observable List items = toObservable([]);
+  @observable var orderedIndex = "content1";
+
+  void goTo(Event e) {
+    print("wentTo");
+  }
 }

954775aa62e1ad69896470c9da07d70519604308
pjv <ezelspinguin@gmail.com>
2014-11-24 22:58:38 +0100

    Working animation.

--- lib/single_strength_item.dart
+++ lib/single_strength_item.dart
@@ -6,36 +6,52 @@ import 'dart:html';
 @CustomTag('single-strength-item')
 class SingleStrengthItem extends PolymerElement {
   SingleStrengthItem.created() : super.created();
+
+  domReady() {
+    animation = shadowRoot.querySelector('#slide-down');
+    animation.target = shadowRoot.querySelector('#desc');
+    descEl = shadowRoot.querySelector('#desc');
+  }
+  CoreAnimationGroup animation;
+  HtmlElement descEl;
+  bool hidden = true;
   @published String title = "Software impact analysis";
   @published String desc = "Document that describes software impact analysis.";
   @published int number = 1;
 
   void toggleDesc(Event e){
-    HtmlElement desc = shadowRoot.querySelector('#desc');
     //desc.style.opacity = (desc.style.opacity == "1") ? "0" : "1";
 
-    CoreAnimationGroup animation = shadowRoot.querySelector('#slide-down');
-    animation.target = shadowRoot.querySelector('#desc');
-    var animationtarget = animation.target;
+    //var animationtarget = animation.target;
     //print("target $animationtarget");
     animation.direction = (animation.direction == "reverse") ? "normal" : "reverse";
-    bool hidden = desc.hidden;
+    hidden = descEl.hidden;
 
-    print("before op: "+desc.style.opacity.toString());
     if (hidden){
-      desc.hidden = !desc.hidden;
-      print("before unhide dir: "+animation.direction.toString()+" op: "+desc.style.opacity.toString());
+      //desc.style.opacity="0";
+      descEl.hidden = !descEl.hidden;
+      print("show");
+      //print("before unhide dir: "+animation.direction.toString()+" op: "+desc.style.opacity.toString());
     }else{
-      desc.style.opacity="1";
+      //desc.style.opacity="1";
     }
+    print("before dir: "+animation.direction.toString()+" op: "+descEl.style.opacity.toString());
     animation.play();
+  }
+
+  void finishToggleDesc(Event e){
     if (!hidden){
-      desc.hidden = !desc.hidden;
-      print("after hide dir: "+animation.direction.toString()+" op: "+desc.style.opacity.toString());
+      //desc.style.opacity="0";
+      descEl.hidden = !descEl.hidden;
+      print("hide");
+      //print("after hide dir: "+animation.direction.toString()+" op: "+desc.style.opacity.toString());
     }else{
-      desc.style.opacity="1";
+      //desc.style.opacity="1";
     }
-    print("after op: "+desc.style.opacity.toString());
+    //print("after");
+    //desc.style.zIndex = "-1";
+    //print("after dir: "+animation.direction.toString()+" op: "+desc.style.opacity.toString());
+    //hidden = !hidden;
   }
 
   void setDescHidden(){

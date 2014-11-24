d6b03874c0ae9c095318b38092f576d752b8d531
pjv <ezelspinguin@gmail.com>
2014-11-24 22:21:00 +0100

    Experiments with animations

--- lib/single_strength_item.dart
+++ lib/single_strength_item.dart
@@ -1,26 +1,44 @@
 
 import 'package:polymer/polymer.dart';
+import 'package:core_elements/core_animation_group.dart';
 import 'dart:html';
 
 @CustomTag('single-strength-item')
 class SingleStrengthItem extends PolymerElement {
   SingleStrengthItem.created() : super.created();
-
-    //animation.target = document.getElementById('desc');
-
-  //var animation = document.getElementById('slide-up');
   @published String title = "Software impact analysis";
   @published String desc = "Document that describes software impact analysis.";
   @published int number = 1;
 
   void toggleDesc(Event e){
     HtmlElement desc = shadowRoot.querySelector('#desc');
-    //desc.classes.toggle('hidden');
-    desc.hidden = !desc.hidden;
-    //animation.play();
+    //desc.style.opacity = (desc.style.opacity == "1") ? "0" : "1";
+
+    CoreAnimationGroup animation = shadowRoot.querySelector('#slide-down');
+    animation.target = shadowRoot.querySelector('#desc');
+    var animationtarget = animation.target;
+    //print("target $animationtarget");
+    animation.direction = (animation.direction == "reverse") ? "normal" : "reverse";
+    bool hidden = desc.hidden;
+
+    print("before op: "+desc.style.opacity.toString());
+    if (hidden){
+      desc.hidden = !desc.hidden;
+      print("before unhide dir: "+animation.direction.toString()+" op: "+desc.style.opacity.toString());
+    }else{
+      desc.style.opacity="1";
+    }
+    animation.play();
+    if (!hidden){
+      desc.hidden = !desc.hidden;
+      print("after hide dir: "+animation.direction.toString()+" op: "+desc.style.opacity.toString());
+    }else{
+      desc.style.opacity="1";
+    }
+    print("after op: "+desc.style.opacity.toString());
   }
 
-  /*void setDescHidden(){
+  void setDescHidden(){
 
-  }*/
+  }
 }

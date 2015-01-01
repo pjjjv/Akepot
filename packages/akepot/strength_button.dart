e0128413275746d24672dc3003a7449ac7e39009
pjv <ezelspinguin@gmail.com>
2015-01-01 19:36:37 +0100

    Site v1 minified

--- packages/akepot/strength_button.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:polymer/polymer.dart';
-
-@CustomTag('strength-button')
-class StrengthButton extends PolymerElement with ChangeNotifier  {
-  StrengthButton.created() : super.created();
-  @reflectable @published int get number => __$number; int __$number = 0; @reflectable set number(int value) { __$number = notifyPropertyChange(#number, __$number, value); }
-  @reflectable @published bool get selected => __$selected; bool __$selected = false; @reflectable set selected(bool value) { __$selected = notifyPropertyChange(#selected, __$selected, value); }
-}

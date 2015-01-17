73ddf72ab2eabf50d5573e9cad98591528c36826
pjv <ezelspinguin@gmail.com>
2015-01-17 13:58:54 +0100

    Triangle images as headers

--- lib/category_pane.dart
+++ lib/category_pane.dart
@@ -1,11 +1,90 @@
 
 import 'package:polymer/polymer.dart';
 import 'package:akepot/model/model_subcategory.dart';
+import 'package:akepot/subcategory_card.dart';
 import 'dart:html';
+import 'package:core_elements/core_ajax_dart.dart';
+import 'dart:math';
+import 'dart:convert';
 
 @CustomTag('category-pane')
 class CategoryPane extends PolymerElement {
   CategoryPane.created() : super.created();
 
   @published List<SubCategory> subcategories;
+  @observable List<Palette> palettes = [];
+  CoreAjax ajaxColourSchemes;
+
+  void domReady() {
+    ajaxColourSchemes = shadowRoot.querySelector('#ajax-colour-schemes');
+    getColourSchemes();
+  }
+
+  void getColourSchemes(){
+    Map map = {};
+    map['numResults'] = 100;
+
+    ajaxColourSchemes.body = JSON.encode(map);
+    print("url: ${ajaxColourSchemes.url}, body: ${ajaxColourSchemes.body}");
+    ajaxColourSchemes.onCoreResponse.listen(ajaxColourSchemesResponse);
+    ajaxColourSchemes.go();
+  }
+
+// Retrieves colour palettes using the Colourlovers API, creating a new Palette
+// for each
+  @reflectable
+  void ajaxColourSchemesResponse(CustomEvent event/*, Map detail, CoreAjax node*/) {
+    var response = event.detail['response'];
+    //print("response: $response");
+
+    try {
+      if (response == null) {
+        return;//TODO: error
+      }
+    } catch (e) {
+      return;
+    }
+
+    for (Map palette in (response as List)) {
+      palettes.add(new Palette.fromJson(palette));
+    }
+
+    for (SubCategoryCard card in shadowRoot.querySelectorAll('#card')){
+      card.palette = palettes[pseudoRandomColor(card.subcategory.name)];
+      card.generateBackground();
+    }
+  }
+
+  int pseudoRandomColor(String text){
+    int limit = palettes.length;
+    Random generator = new Random(text.hashCode);
+    int random = generator.nextInt(limit);
+    return random;
+  }
+}
+
+// Class representing a single colour palette made up of multiple colours
+// Colours are simply hex strings
+class Palette extends Observable {
+  final String name;
+  final List<String> colors;
+
+  Palette(this.name, this.colors);
+
+  toString() => name;
+
+  factory Palette.fromJson(Map _json) {
+    String name = "Palette";
+    List<String> colors = [];
+
+    if (_json.containsKey("title")) {
+      name = _json["title"];
+    }
+    if (_json.containsKey("colors")) {
+      colors = _json["colors"].map((color) => "#"+color).toList();
+    }
+
+    Palette palette = new Palette(name, colors);
+    return palette;
+  }
 }
--- lib/subcategory_card.dart
+++ lib/subcategory_card.dart
@@ -1,19 +1,49 @@
 
 import 'package:polymer/polymer.dart';
 import 'package:akepot/model/model_subcategory.dart';
+import 'package:akepot/category_pane.dart';
 import 'package:akepot/single_strength_item.dart';
+import 'package:core_elements/core_image.dart';
 import 'dart:html';
+import 'dart:js';
 
 @CustomTag('subcategory-card')
 class SubCategoryCard extends PolymerElement {
   SubCategoryCard.created() : super.created();
 
   @published SubCategory subcategory;
+  @published Palette palette;
+
+  void domReady() {
+    generateBackground();
+  }
 
   void toggleOtherDescs(Event e) {
     for(SingleStrengthItem item in shadowRoot.querySelectorAll('single-strength-item')){
       print("toggleOtherDescs");
       item.setDescHidden();
     }
+  }
+
+  void generateBackground() {
+    if(palette == null){
+      return;
+    }
+    CoreImage image = shadowRoot.querySelector('#header');
+    if(image.getClientRects().length == 0){
+      return;
+    }
+    Rectangle clientRect = image.getClientRects()[0];
+
+    var trianglifier = new JsObject(context['Trianglify'], [new JsObject.jsify({"bleed": 30,
+      "cellsize": 75,
+      "x_gradient": new JsObject.jsify(palette.colors),
+      //"y_gradient": false,
+      "cellpadding": 15,
+      "noiseIntensity": 0.2
+    })]);
+    var pattern = trianglifier.callMethod('generate', [clientRect.width, clientRect.height]);
+
+    image.src = pattern['dataUri'];
   }
 }

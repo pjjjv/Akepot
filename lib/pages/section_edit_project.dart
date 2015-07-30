079d16442d6d4889bb9f2cefd8c0d59706ab83bb
pjv <ezelspinguin@gmail.com>
2015-07-31 00:08:30 +0200

    dialog back

--- lib/pages/section_edit_project.dart
+++ lib/pages/section_edit_project.dart
@@ -3,6 +3,9 @@ import 'package:polymer/polymer.dart';
 import 'package:akepot/model/model_project.dart';
 import 'package:akepot/model/model_category.dart';
 import 'dart:html';
+import 'package:paper_elements/paper_button.dart';
+import 'package:app_router/app_router.dart';
+import 'package:paper_elements/paper_action_dialog.dart';
 
 @CustomTag("section-edit-project")
 class SectionEditProject extends PolymerElement {
@@ -13,8 +16,28 @@ class SectionEditProject extends PolymerElement {
   @observable Project project;
   @published int page;
   @published int index;
+  @observable String newlink;
 
   void domReady() {
+    PaperButton createButton = shadowRoot.querySelector("#create-button");
+    createButton.hidden = false;
+    createButton.onClick.listen(showDialog);
+  }
+
+  void showDialog(Event e){
+    newlink = window.location.protocol + "//" + window.location.host + window.location.pathname + "#/project/${project.hash}";
+    print("New project link would be: $newlink");
+
+    PaperActionDialog dialog = shadowRoot.querySelector('#created-dialog');
+
+    PaperButton goButton = shadowRoot.querySelector('#go-button');
+    goButton.onClick.first.then(onGoButtonClick);
+
+    dialog.toggle();
+  }
+
+  void onGoButtonClick(Event e){
+    (document.querySelector('app-router') as AppRouter).go("/project/${project.hash}");
   }
 
   void onItemTap(Event e, var detail, HtmlElement target){

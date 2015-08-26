17899edeb984ba7d5b216fe4921dd4d413673e7f
pjv <ezelspinguin@gmail.com>
2015-08-26 23:48:20 +0200

    Style for edit items. Minor text changes.

--- lib/dropdown_menu.dart
+++ lib/dropdown_menu.dart
@@ -1,12 +1,19 @@
 
 import 'package:polymer/polymer.dart';
 import 'dart:html';
+import 'package:akepot/competences_service.dart';
 import 'package:akepot/login_screen.dart';
 import 'package:paper_elements/paper_action_dialog.dart';
 
 @CustomTag('dropdown-menu')
 class DropdownMenu extends PolymerElement {
   DropdownMenu.created() : super.created();
+
+  @observable CompetencesService service;
+
+  domReady(){
+    service = document.querySelector("#service");
+  }
 
   void about(Event e) {
     PaperActionDialog dialog = shadowRoot.querySelector('#about-dialog');
@@ -15,5 +22,7 @@ class DropdownMenu extends PolymerElement {
 
   void signOut(Event e) {
     (document.querySelector("#loginscreen") as LoginScreen).signOut(e);
+    service.signedIn = false;
+    service.user = new User();
   }
 }
